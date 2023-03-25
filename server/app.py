38018d241bb8bc32c0b265791191a980f7772d34
Ziru Zou <jiyucoding@yahoo.com>
2023-03-25 19:51:33 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -53,5 +53,47 @@ api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
 
+# log in
+class Login(Resource):
+    def post(self):
+        # gets a username from request's JSON.
+        user = User.query.filter(User.username==request.get_json()["username"]).first()
+
+        # set value of the session's user_id value to the user's id.
+        session["user_id"] = user.id
+        # returns the user as JSON with a 200 status code.
+        return make_response(jsonify(user.to_dict()), 200)
+
+# stay logging in, even after refreshing the page.
+class CheckSession(Resource):
+    def get(self):
+        # retrieves the user_id value from the session.
+        user = User.query.filter(User.id==session.get("user_id")).first()
+
+        if not user:
+            # if the session does not have a user_id, get() returns no data and a 401 (Unauthorized) status code.
+            return make_response(jsonify({}), 401)
+        # If the session has a user_id, get() returns the user as JSON with a 200 status code.
+        return make_response(jsonify(user.to_dict()), 200)
+
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+
+
+# logout
+class Logout(Resource):
+    # has one route, delete()
+    def delete(self):
+        # delete() removes the user_id value from the session.
+        # set user_id to None when logout
+        session['user_id'] = None
+
+        # returns no data and a 204 (No Content) status code.
+        return make_response(jsonify({'message': "204: No content"}), 204)
+api.add_resource(Logout, "/logout")
+
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
